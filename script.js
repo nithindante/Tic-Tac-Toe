c4f37b0e73d865b326eb40df2f25ddb1cecfee1b
Nithindmt <Rajkumar_nithin@yahoo.com>
2022-07-30 19:31:47 +0530

    Insert basic layout of gameplay and add the Markers as well

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let grids = document.querySelector(".grids");
 let child = grids.childNodes;
+let lines = document.querySelectorAll(".lines");
 let playerPerson = function (name,symbol) {
     return{
         name,
@@ -11,19 +12,42 @@ let gameBoard = (function () {
     let board=[];
     for(let i=0;i<9;i++)
     {
-        board.push("");
+        board.push("X");
     }
-    
-    for ( let i=0;i<board.length;i++)
+    console.log(board);
+    for (let i=0;i<board.length;i++)
     { 
-    const lines = document.createElement("div");
+    let lines = document.createElement("div");
     lines.classList.add("lines")
-    board[i]=lines.innerHTML;
     grids.appendChild(lines);
+    board[i]=lines;
+    }
+
+    function setCall(index,symbol) 
+    {  
+        if((board[index-1].innerHTML===""))
+        {
+            board[index-1].innerHTML=game.currentplayer.symbol;
+            return true;
+        }
+        else
+        return false;
+    }
+    function win()
+    {
+
+    }
+    function draw()
+    {
+
     }
 
     return{
         board,
+        lines,
+        setCall,
+        win,
+        draw,
     };
 })();
 
@@ -31,13 +55,50 @@ let game = (()=>{
     let player1 = playerPerson("Mike","X");
     let player2 = playerPerson("Robin","O");
     let currentplayer = player1;
-    child.forEach((div)=>{
-        div.addEventListener("click", function(){
-            console.log("andi");
-            div.innerHTML="X";
+    let switchPlayer = function(){   
+        if(currentplayer===player1)
+        {
+         currentplayer=player2 ;
+         return currentplayer;
+        }
+        else
+        {
+            currentplayer= player1;
+            return currentplayer;
+        }
+    }
+    function win () {
+        console.log(gameBoard.board)
+      return true;
+    }
+ 
+    let draw = function () {
+        
+    }
+    child.forEach((div,index)=>{
+        div.addEventListener("click", function () { 
+            if(gameBoard.setCall(index,currentplayer.symbol))
+            {
+                div.innerHTML=currentplayer.symbol;
+                win();
+                {
+
+                }
+                if(draw)
+                {
+
+                }
+                switchPlayer();
+            }
         });
     });
     
-   
-    
+    return{
+        player1,
+        player2,
+        switchPlayer,
+        currentplayer,
+        win,
+        draw,
+    };
 })();
